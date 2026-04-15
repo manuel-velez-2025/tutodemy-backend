ed4d217b16772b4bb6aa07ca9a1ee5b426ae79e9
Manuel Velez <243775@ids.upchiapas.edu.mx>
2026-04-15 15:43:48 -0600

    fix: agregar verificarToken en GET /api/clases

--- src/routes/clasesRoutes.js
+++ src/routes/clasesRoutes.js
@@ -4,12 +4,11 @@ const clasesController = require('../controllers/clasesController');
 const verificarToken = require('../middlewares/authMiddleware');
 
 router.post('/', verificarToken, clasesController.crearClase);
-router.get('/', clasesController.obtenerClases);
+router.get('/', verificarToken, clasesController.obtenerClases);
 router.get('/creadas', verificarToken, clasesController.obtenerMisClasesCreadas);
+router.get('/mis-alumnos', verificarToken, clasesController.obtenerMisAlumnosGeneral);
 router.get('/:id/alumnos', verificarToken, clasesController.obtenerAlumnosPorClase);
 router.delete('/:id', verificarToken, clasesController.eliminarClase);
 router.put('/:id', verificarToken, clasesController.actualizarClase);
-router.get('/mis-alumnos', verificarToken, clasesController.obtenerMisAlumnosGeneral);
-router.get('/', verificarToken, clasesController.obtenerClases);
 
 module.exports = router;
